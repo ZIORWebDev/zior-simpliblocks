b3ac6a17eab9834c0953daac9b5c7388025b1438
Rey C <reygcalantaol@gmail.com>
2026-02-27 21:55:53 +0800

    Release 1.0.3

--- zior-simpliblocks.php
+++ zior-simpliblocks.php
@@ -5,7 +5,7 @@
  * Description:       A collection of simple, lightweight blocks focused on clarity and performance.
  * Author:            ZIORWeb.Dev
  * Author URI:        https://ziorweb.dev
- * Version:           1.0.2
+ * Version:           1.0.3
  * Requires at least: 6.0
  * Requires PHP:      8.0
  * License:           GPL-2.0-or-later
